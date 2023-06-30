dd0bf42f01c9a34f519db21cd7cb768c6ab46d31
Juha Siivikko <juha@siivikko.fi>
2023-06-30 14:10:01 +0200

    Add function to call merchant_order

--- config-sample.py
+++ config-sample.py
@@ -1,3 +1,4 @@
 ghost_admin_apikey = ''
-api_url = ''
 confirmations = 2
+BLOCKONOMICS_MERCHANT_ORDER_URL = 'https://www.blockonomics.co/api/merchant_order/'
+blockonomics_api_key = ''
--- main.py
+++ main.py
@@ -6,17 +6,27 @@ import config
 
 app = FastAPI()
 
-@app.post("/hook/")
+async def fetch_merchant_order(client, uuid):
+  headers = {
+    'Authorization': 'Bearer {}'.format(config.blockonomics_api_key),
+    'Content-Type': 'application/json'
+  }
+  r = await client.get('{}{}'.format(config.BLOCKONOMICS_MERCHANT_ORDER_URL, uuid), headers=headers)
+  merchant_data = r.json().get('data')
+  return merchant_data.get('emailid'), merchant_data.get('Custom Field1')
+
+@app.get("/hook/")
 async def test_hook(status: int, uuid):
   if status >= config.confirmations:
     async with httpx.AsyncClient() as client:
+      customer_email, ghost_url = await fetch_merchant_order(client, uuid)
       token = JwtUtil.gen_jwt()
       headers = {
         'Authorization': 'Ghost {}'.format(token),
         'Accept-Version': 'v5.0',
         'Content-Type': 'application/json'
       }
-      data = {"members": [{"email": "test1@ghost.org"}]}
-      r = await client.post("{}/ghost/api/admin/members/".format(config.api_url), json=data, headers=headers)
+      data = {"members": [{"email": customer_email}]}
+      r = await client.post("{}/ghost/api/admin/members/".format(ghost_url), json=data, headers=headers)
       return r.json()
   return {}
